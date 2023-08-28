808f31c3c9eec03b74cbc032562b902fdf6d21d6
Connor Hassan <connorhassan@hotmail.co.uk>
2023-08-28 17:01:34 +0100

    Created basic skeleton framework structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='and_theyre_off',
+      version="1.0",
+      description="Horse racing predictive model",
+      license="MIT",
+      author="Josh Stone",
+      #url="https://github.com/lewagon/taxi-fare",
+      install_requires=requirements,
+      packages=find_packages(),
+      test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
