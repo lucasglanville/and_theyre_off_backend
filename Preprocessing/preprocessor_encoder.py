b8b79e6154742ebea9422b90e2d4fca1c38218c6
Connor Hassan <connorhassan@hotmail.co.uk>
2023-08-30 16:20:43 +0100

    Re adjusted the preprocessor func to drop columns and Nulls| Created basic NN model

--- Preprocessing/preprocessor_encoder.py
+++ Preprocessing/preprocessor_encoder.py
@@ -1,18 +1,23 @@
-import numpy as np
-import pandas as pd
 
 from sklearn import set_config
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 
-def preprocess_features(X: pd.DataFrame) -> np.ndarray:
+def preprocess_features(data: pd.DataFrame) -> np.ndarray:
+
+    ### dropping columns ###
+    def drop_columns(x):
+      x.drop(columns=['id.1', 'f_id.1','f_place','id','f_id', 'f_ko',
+                      'f_bsp_p_back', 'f_bsp_p_lay',	'f_pm_01m_p_back',
+                      'f_pm_01m_p_lay','f_pm_15m_p_back',	'f_pm_15m_p_lay',
+                      'f_ip_min', 'f_ip_max'])
 
     ### CONVERT ODDS TO PROBABILITY ###
     def odds_to_prob(x):
         return 1/x
-    X['pred_isp'] = X['pred_isp'].apply(odds_to_prob)
-    X['f_pm_01m'] = X['f_pm_01m'].apply(odds_to_prob)
+    data['pred_isp'] = data['pred_isp'].apply(odds_to_prob)
+    data['f_pm_01m'] = data['f_pm_01m'].apply(odds_to_prob)
     print("✅ ODDS CONVERTED TO PROBABILITY (1/ODDS)")
 
     ### CODE WINNERS AS '1', REST AS '0' ###
@@ -21,11 +26,11 @@ def preprocess_features(X: pd.DataFrame) -> np.ndarray:
             return 1
         else:
             return 0
-    X['f_place'] = X['f_place'].apply(winner)
+    data['f_place'] = data['f_place'].apply(winner)
     print("✅ WINNERS CODED AS '1'")
 
     ### 'f_ko' CONVERTED TO DATETIME ###
-    X['f_ko'] = X['f_ko'].astype('datetime64[ns]')
+    data['f_ko'] = data['f_ko'].astype('datetime64[ns]')
     print("✅ 'f_ko' CONVERTED TO DATETIME")
 
     ### ENCODE THE GOING (GROUND CONDITION) ###
@@ -43,13 +48,13 @@ def preprocess_features(X: pd.DataFrame) -> np.ndarray:
         elif x == 'HVY' or x == 'HTS':
             return 6
 
-    X['f_going'] = X['f_going'].apply(f_going_coder)
+    data['f_going'] = data['f_going'].apply(f_going_coder)
     print("✅ TRACK CONDITIONS ORDINALLY ENCODED")
 
     ### DROP ROWS WITH NULL VALUES IN THESE COLUMNS ###
-    X.dropna(axis = 0,
+    data.dropna(axis = 0,
             inplace = True,
-            subset = ['f_racetype', 'f_jockey',
+            subset = ['f_racetype', 'f_jockey', 'f_class',
                     'f_trainer', 'f_pace',
                     'f_rating_or'])
     print("✅ DROPPED ROWS WITH NULL VALUES")
@@ -59,7 +64,9 @@ def preprocess_features(X: pd.DataFrame) -> np.ndarray:
     numeric_features = ["f_distance", "f_class",
                         "f_age", "f_pace", "f_weight",
                         "f_runners", "f_rating_rbd",
-                        "f_rating_or"]
+                        "f_rating_or", "f_going", 'f_bsp',
+                        'f_pm_15m',	'f_pm_10m',	'f_pm_05m',
+                        'f_pm_03m',	'f_pm_02m']
     numeric_transformer = Pipeline(
         steps=[("scaler", MinMaxScaler())])
     print("✅ NUMERIC FEATURES MINMAX-SCALED")
@@ -83,7 +90,7 @@ def preprocess_features(X: pd.DataFrame) -> np.ndarray:
 
     ### FIT_TRANSFORM FEATURES ###
     print("⏳ FIT_TRANSFORMING FEATURES...")
-    X_processed = preprocessor.fit_transform(X)
-    print("✅ X_PROCESSED WITH SHAPE:", X_processed.shape)
+    data_processed = preprocessor.fit_transform(data)
+    print("✅ X_PROCESSED WITH SHAPE:", data_processed.shape)
 
-    return X_processed
+    return data_processed
